8fa43e28929ea4c27c5ad905966a786f0ccbb819
nemliProg <younesbuzz@gmail.com>
2022-11-22 22:53:28 +0100

    Main file to test the entities if it works fine

---
+++ gestion_livraison/src/main/java/com/glivraison/gestion_livraison/main.java
@@ -0,0 +1,22 @@
+package com.glivraison.gestion_livraison;
+
+import com.glivraison.gestion_livraison.entities.CategorieVehicle;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class main {
+
+        public static void main(String[] args) {
+
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestion_livraison");
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+            CategorieVehicle categorieVehicle = new CategorieVehicle();
+            categorieVehicle.setType("Camion");
+            em.persist(categorieVehicle);
+            em.getTransaction().commit();
+            em.close();
+            emf.close();
+        }
+}
