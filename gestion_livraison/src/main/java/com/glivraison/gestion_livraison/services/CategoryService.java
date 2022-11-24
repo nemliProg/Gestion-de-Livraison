7564f534a3e090f07f147348081ebd009c193d2d
nemliProg <younesbuzz@gmail.com>
2022-11-24 19:49:34 +0100

    backend packages structured

---
+++ gestion_livraison/src/main/java/com/glivraison/gestion_livraison/services/CategoryService.java
@@ -0,0 +1,9 @@
+package com.glivraison.gestion_livraison.services;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+
+@Named
+@ApplicationScoped
+public class CategoryService {
+}
